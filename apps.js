ad2dad8c8b8d3ddcce9a8cd49e3bd620940e7e46
waduh123 <105790647+waduh123@users.noreply.github.com>
2022-05-19 20:21:17 +0700

    Add files via upload

--- apps.js
+++ apps.js
@@ -1,21 +1,10 @@
 const http = require('http')
-const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.writehead(200, { 'Content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data){
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else{
-            res.write(data)
-        }
-        res.end() 
-    })
-    })  
     res.write('Hello Node')
     res.end()
+})
 
 server.listen(port ,function(error) {
     if (error) {
